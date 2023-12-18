e6d0fc9d9d7bd747a8488d2465be4efbf794811f
Osmar Liera <80195420+OsmarLG@users.noreply.github.com>
2023-12-18 01:41:43 -0700

    main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,18 +6,19 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        @endif
+                        {{ __('You are logged in!') }}
+                    </div>
                 </div>
+                <br>
+                <a href="{{ route('products.index') }}" class="btn btn-primary" role="button">Products</a>
+
             </div>
-        </div>
     </div>
 </div>
 @endsection
